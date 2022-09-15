ca83c400cc89a0de7a0f7c15f9a26f566f9cc6e2
annafromthecfam <anna@myclarkfamily.org>
2022-09-15 12:53:16 -0600

    Ode to Joy

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int A = 440;
+int B = 494;
+int C = 262;
+int D = 294;
+int E = 330;
+int F = 349;
+int G = 392;
+int quarterNote = 600;
+int halfNote = 1200;
+int dottedHalfNote = 1500;
+int wholeNote = 1400;
+
+//Measure 1
+Console.Beep(E,quarterNote);
+Console.Beep(E,quarterNote);
+Console.Beep(F,quarterNote);
+Console.Beep(G,quarterNote);
+//Measure 2
+Console.Beep(G,quarterNote);
+Console.Beep(F,quarterNote);
+Console.Beep(E,quarterNote);
+Console.Beep(D,quarterNote);
+//Measure 3
+Console.Beep(C,quarterNote);
+Console.Beep(C,quarterNote);
+Console.Beep(D,quarterNote);
+Console.Beep(E,quarterNote);
+//Measure 4
+Console.Beep(E,quarterNote);
+Console.Beep(D,quarterNote);
+Console.Beep(D,halfNote);
+//Measure 5
+Console.Beep(E,quarterNote);
+Console.Beep(E,quarterNote);
+Console.Beep(F,quarterNote);
+Console.Beep(G,quarterNote);
+//Measure 6
+Console.Beep(G,quarterNote);
+Console.Beep(F,quarterNote);
+Console.Beep(E,quarterNote);
+Console.Beep(D,quarterNote);
+//Measure 7
+Console.Beep(C,quarterNote);
+Console.Beep(C,quarterNote);
+Console.Beep(D,quarterNote);
+Console.Beep(E,quarterNote);
+//Measure 8
+Console.Beep(D,quarterNote);
+Console.Beep(C,quarterNote);
+Console.Beep(C,halfNote);
